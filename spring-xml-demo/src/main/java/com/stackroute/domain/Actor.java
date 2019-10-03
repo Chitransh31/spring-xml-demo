a0e7d1d19e623c0f7ebbfa878c133339a1ceb1c1
Chitransh31 <Saurabhbagade101@gmail.com>
2019-10-03 10:42:24 +0530

    "Task 2 First Commit"

--- spring-xml-demo/src/main/java/com/stackroute/domain/Actor.java
+++ spring-xml-demo/src/main/java/com/stackroute/domain/Actor.java
@@ -9,6 +9,12 @@ public class Actor {
         System.out.println("Actor Object constructed!");
     }
 
+    Actor(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
     public String getName() {
         return this.name;
     }
