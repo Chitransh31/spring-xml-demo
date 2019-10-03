f75f71ad9dfdd56bac86cba90491ce9b531ed150
Chitransh31 <Saurabhbagade101@gmail.com>
2019-10-03 13:01:21 +0530

    "Task 6 First Commit"

--- spring-xml-demo/src/main/java/com/stackroute/Main.java
+++ spring-xml-demo/src/main/java/com/stackroute/Main.java
@@ -1,23 +1,19 @@
 package com.stackroute;
 
-import com.stackroute.demo.BeanLifecycleDemoBean;
-import org.springframework.context.ConfigurableApplicationContext;
+import com.stackroute.demo.BeanPostProcessorDemoBean;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext context =
-                new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        BeanLifecycleDemoBean beanLifecycleDemoBean = (BeanLifecycleDemoBean) context.getBean
-                ("beanLifecycleDemoBean");
-        System.out.println(beanLifecycleDemoBean);
-        beanLifecycleDemoBean.customInit();
-        beanLifecycleDemoBean.customDestroy();
+        BeanPostProcessorDemoBean obj = (BeanPostProcessorDemoBean) context.getBean
+                ("beanPostProcessorDemoBean");
 
-        context.close();
+        context.registerShutdownHook();
 
     }
 }
